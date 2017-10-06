2913a22cbe2b8b7d248cf7ad5c3ea1bf80467b1f
Marat Bekkulov <marat@gmail.com>
2017-10-06 19:41:48 +0600

    first commit

---
+++ db/migrate/20171006132656_create_dishes.rb
@@ -0,0 +1,12 @@
+class CreateDishes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :dishes do |t|
+      t.string :name
+      t.integer :price
+      t.text :description
+      t.references :restaurant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
